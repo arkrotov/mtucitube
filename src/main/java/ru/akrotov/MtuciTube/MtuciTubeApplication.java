15b027162b70649dd5b377f2f7025ae318f9cc4e
Artem Krotov <artkrotov7@gmail.com>
2017-12-18 09:24:23 +0300

    init

---
+++ src/main/java/ru/akrotov/MtuciTube/MtuciTubeApplication.java
@@ -0,0 +1,12 @@
+package ru.akrotov.MtuciTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtuciTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtuciTubeApplication.class, args);
+	}
+}
